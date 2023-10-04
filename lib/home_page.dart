422f38b0a6ce57e5fd5e84db24b2b6a9e1975612
Bruna Magalhães Criscuolo <bruna.magalhaes@levva.io>
2023-10-04 14:05:22 -0300

    feat: create home controller

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/home_controller.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var controller =
+        context.dependOnInheritedWidgetOfExactType<HomeController>()!;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+      ),
+      body: Center(
+        child: Text('Fluterando ${controller.value}'),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
